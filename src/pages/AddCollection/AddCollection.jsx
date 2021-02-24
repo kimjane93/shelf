6503018c7bd3325313c740ab1d4062bdd967f290
mcholbrook <mirandacholbrook@gmail.com>
2021-02-24 13:31:04 -0500

    Add semanticUI and add collection form

--- src/pages/AddCollection/AddCollection.jsx
+++ src/pages/AddCollection/AddCollection.jsx
@@ -1,13 +1,56 @@
 import React, { Component, useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import {Form} from 'semantic-ui-react'
 
-
-function AddCollection(){
-    return (
-        <h1>This is the Add Collection Page</h1>
-    )
+let blankFormData = {
+  title: "",
+  description: "",
+  type: "",
 }
 
+class AddCollection extends Component {
+  state = { 
+    invalidForm: true,
+    formData: {
+      title: "",
+      description: "",
+      type: "",
+    }
+   }
 
+  formRef = React.createRef()
 
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.props.handleAddCollection(this.state.formData);
+    this.setState((state) => ({
+      formData: blankFormData
+    }))
+  };
+
+  handleChange = (e) => {
+    const formData = {
+      ...this.state.formData,
+      [e.target.name]: e.target.value,
+    };
+    this.setState({
+      formData,
+      invalidForm: !this.formRef.current.checkValidity(),
+    });
+  };
+
+  render() { 
+    return ( 
+      <>
+        <h3>Add Collection</h3>
+          <Form>
+              <Form.Input fluid label='Title' placeholder='Title' name='title' value={this.state.formData.title} onChange={this.handleChange} required />
+              <Form.Input fluid label='Description' placeholder='Description' name='description' value={this.state.formData.title} onChange={this.handleChange} required />
+            <Form.Button>Submit</Form.Button>
+          </Form>
+      </>
+     );
+  }
+}
+ 
 export default AddCollection;
