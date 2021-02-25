76c2190ca8c4d436dbf5aa4a31b513212ef6552e
mcholbrook <mirandacholbrook@gmail.com>
2021-02-25 13:08:29 -0500

    Add collections to profile page

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -1,10 +1,21 @@
+import { PromiseProvider } from "mongoose";
 import React, { Component, useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
 
-function Profile(){
+function Profile({collections}){
     return (
-        <h1>This is the Profile Page</h1>
+        <>
+          <h1>Profile</h1>
+          <div>
+            {collections.map((collection) => (
+              <>
+              <p>{collection.title}</p>
+              <p>{collection.description}</p>
+              </>
+            ))}
+          </div>
+        </>
     )
 }
 
