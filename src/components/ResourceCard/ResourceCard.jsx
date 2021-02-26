09b73cf8b7a0478fbcee6555b0383217a7bf3209
kimjane93 <kjchadwell93@gmail.com>
2021-02-26 10:19:04 -0500

    add conditional classNames based on type to resource

--- src/components/ResourceCard/ResourceCard.jsx
+++ src/components/ResourceCard/ResourceCard.jsx
@@ -4,7 +4,8 @@ import { Link } from "react-router-dom";
 function ResourceCard({resource, handleDeleteResourceFromCollection}) {
   return (
     <>
-      <div className="card text-dark bg-light mb-3" style={{'max-width': '18rem'}}>
+      <div className={`card text-dark ${resource.type === 'website' ? 'bg-light': resource.type === 'audio' ? 'bg-primary' : resource.type === 'graphic' ? 'bg-secondary' : resource.type === 'book' ? 'bg-warning' : resource.type === 'article' ? 'bg-success' : resource.type === 'video' ? 'bg-danger' : 'bg-info'} mb-3`}
+      style={{'max-width': '18rem'}}>
         <div class="card-header"><a href={resource.url}>{resource.title}</a></div>
         <div className="card-body">
           <h5 className="card-title">Type: {resource.type}</h5>
