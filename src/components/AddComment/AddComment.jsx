d43172f119e9084b387d8f6ce02682fddf05d073
kimjane93 <kjchadwell93@gmail.com>
2021-02-27 09:25:13 -0500

    stub up handleSubmit and handleChange on commment component

--- src/components/AddComment/AddComment.jsx
+++ src/components/AddComment/AddComment.jsx
@@ -1,12 +1,6 @@
 import React, { Component } from 'react'
 
 
-let blankForm 
-
-
- 
-    
-
 class AddComment extends Component {
     state = {
         invalidForm: true,
@@ -19,6 +13,21 @@ class AddComment extends Component {
     }
 
     formRef = React.formRef()
+
+    handleSubmit = (e) => {
+        e.preventDefault()
+        this.props.handleAddComment(this.state.formData)
+        this.setState({ formData: {content: ''} })
+    }
+
+    handleChange = (e) => {
+        const formData = {
+            ...this.state.formData, [e.target.name]: e.target.value
+        }
+        this.setState({ formData: formData, invalidForm: !this.formRef.current.checkValidity()
+        })
+    }
+
 
     render(){
         return (
