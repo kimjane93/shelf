d18347bd2ab0ccd3e17f2a63decd0ea893ae41cf
mcholbrook <mirandacholbrook@gmail.com>
2021-03-08 18:32:23 -0500

    Fix navbar bug issue on login page

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -6,29 +6,33 @@ const NavBar = ({ user, handleLogout }) => {
     <>
       {user ?
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <a className="navbar-brand" href="/">Shelf</a>
-        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-          <div className="navbar-nav ml-auto">
-            <a className="nav-item nav-link" href="/addresource">Add Resource</a>
-            <a className="nav-item nav-link" href="/addcollection">Add Collection</a>
-            <a className="nav-item nav-link" href="/search">Search</a>
-            <a className="nav-item nav-link" href="/chat">Chat</a>
-            <a className="nav-item nav-link" href="/users">Other Shelfers</a>
-            <a className="nav-item nav-link" href="/myprofile"  aria-disabled="true">My Profile</a>
-            <a className="nav-item nav-link" href=" " onClick={handleLogout}>Log Out</a>
+          <a className="navbar-brand" href="/">Shelf</a>
+          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div className="navbar-nav ml-auto">
+              <a className="nav-item nav-link" href="/addresource">Add Resource</a>
+              <a className="nav-item nav-link" href="/addcollection">Add Collection</a>
+              <a className="nav-item nav-link" href="/search">Search</a>
+              <a className="nav-item nav-link" href="/chat">Chat</a>
+              <a className="nav-item nav-link" href="/users">Other Shelfers</a>
+              <a className="nav-item nav-link" href="/myprofile"  aria-disabled="true">My Profile</a>
+              <a className="nav-item nav-link" href=" " onClick={handleLogout}>Log Out</a>
+            </div>
           </div>
-        </div>
       </nav>
       :
-        <nav>
-          <div className="nav-wrapper">
-            <ul id="nav-mobile" className="right">
-              <li><Link to="/login" className="nav-link">Log In</Link></li>
-              <li><Link to="/signup" className="nav-link">Sign Up</Link></li>
-            </ul>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+          <a className="navbar-brand" href="/">Shelf</a>
+          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div className="navbar-nav ml-auto">
+              <a className="nav-item nav-link" href="/login">Log In</a>
+              <a className="nav-item nav-link" href="/signup">Sign Up</a>
+            </div>
           </div>
         </nav>
       }
